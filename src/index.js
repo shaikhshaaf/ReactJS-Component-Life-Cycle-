6947d67aac505cd314b667069022917b5adb0921
shaikh@SHAAF <afsarshaikhsk89@gmail.com>
2019-03-02 21:43:30 +0530

    component life cycle

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+
+ class App extends React.Component {
+     constructor(props){
+         super(props);
+         this.state={
+             count:0
+         }
+      console.log("constructor");
+     }
+
+ incCount = () =>{
+     this.setState({
+         count:this.state.count +1
+     })
+ }
+
+ componentWillMount(){
+    console.log("will mount");
+ }
+
+
+
+ shouldComponentUpdate(){
+     if(this.state.count > 10) {
+         return false;
+     }
+     return true;
+ }
+
+ componentDidMount(){
+    console.log("mounted");
+ }
+
+
+
+  render() {
+    console.log("renderring");
+    return (
+      <section>
+          {this.state.count}
+           <button onClick={this.incCount} > increment </button>
+      </section>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
